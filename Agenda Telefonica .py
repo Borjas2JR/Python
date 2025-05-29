e7c8d617e0587f18614132fef6dfb8d232338fa5
Borjas2JR <ricardoborjas31@gmail.com>
2025-05-29 02:32:25 +0200

    Add files via upload

---
+++ Agenda Telefonica .py
@@ -0,0 +1,34 @@
+print("Esta en una agenda para guardar o buscar numeros telefonicos")
+
+agenda = [
+    ["Ricardo", "Josselin", "Kelyn"],
+    ["632 878 849", "633 767 555", "681 614 860"]
+]
+
+def function_agendar(agenda):
+    entrada = input("Ingresa el nombre: ")
+    entrada2 = input("Ingresa el telefono:")
+
+    if entrada != "":
+        if entrada2 != "":
+            agenda[0].append(entrada)
+            agenda[1].append(entrada2)
+
+def function_buscar(agenda):
+    name1 = input("Que contacto deseas buscar?")
+
+    if name1 in agenda[0]:
+        index = agenda[0].index(name1)
+        telefono = agenda[1][index]
+
+        print("El contacto es: ", name1, "Telefono: ", telefono )
+
+name = input("Que desea a hacer? b/buscar un contacto o g/guardar un contacto")
+
+if name == "b":
+    function_buscar(agenda)
+elif name == "g":
+    function_agendar
+else:
+    print("Ingresa un valor correcto")
+    
